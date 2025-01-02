aad1b7e07fa3df9dadf45db508fa08260e785f8e
Paula-JS <paulaajs20@gmail.com>
2025-01-02 19:29:15 +0100

    error 4

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     # Path del modelo preentrenado
-    MODEL_PATH = "modelo_prediccion_precios_casas_prefab.pkl"
-    SCALER_PATH = "scaler.pkl"
+    MODEL_PATH = "modelo_xgb.pkl"
+    SCALER_PATH = "scaler_xgb.pkl"
 
     # Función que devuelve la predicción
     def model_prediction(x_in, model, scaler):
